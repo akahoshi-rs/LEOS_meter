fc75ab07efbcf57213315367dc488edc7c560071
akahoshi-rs <akahoshi.0620@gmail.com>
2021-08-05 11:52:37 +0900

    0805

--- background.js
+++ background.js
@@ -1,7 +1,19 @@
-chrome.runtime.onInstalled.addListener(function() {
-  //拡張機能がインストールされたときや更新された時に関数内を実行する
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    //storage APIの命令
-      console.log("The color is green.");
-  });
-});
+console.log('LEOS_meter is running.');
+
+// let head_container = document.querySelector('#container.ytd-masthead');
+// head_container.style.justifyContent = 'start';
+
+// let head_searchForm = document.querySelector('#center.ytd-masthead');
+// head_searchForm.style.flex = 'unset';
+
+// let meter = document.createElement('div');
+
+
+const audioContext = new OfflineAudioContext(2, 44100 * 40, 44100);
+
+const audioElement = document.querySelector('video');
+// console.log(document.querySelector('iframe').contentWindow.document);
+console.log(audioElement);
+const track = audioContext.createMediaElementSource(audioElement);
+
+console.log(track);
